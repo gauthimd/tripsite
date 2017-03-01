aa8ff23328b674b34ca47d8a8a82263522c17286
Mike Gauthier <gauthiermichael3.14@gmail.com>
2017-03-01 09:37:46 -0600

    First commit for WxPage

---
+++ README.md
@@ -0,0 +1,2 @@
+### Raspberry Pi 3 WxPage
+This folder contains all files pertaining to the Wx website for the weather pi project
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Raspberry Pi Weather Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <meta charset="UTF-8">
+  </head>
+  <div class="page">
+  <body>
+    <h1>Weather from Lake Tyler East</h1>
+    <p>The following weather data was obtained using a Raspberry Pi 3 with 
+         sensors and components from SwitchDocLabs</p><br>
+    <table>
+      <tr>
+	<th>Date/Time</th>
+	<th>Temperature</th>
+	<th>Pressure</th>
+	<th>AvgWindSpeed</th>
+	<th>Humidity</th>
+	<th>WindDirection</th>
+	<th>Rainfall</th>
+	<th>WindChill</th>
+	<th>HeatIndex</th>
+      </tr>
+      <tr>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+      </tr>
+    </table><br>
+    <h2>Station Images</h2>
+    <div class="images">
+    <img id="left" src="images/PiStation.jpg">
+    <img id="center" src="images/PiStation2.jpg">
+    <img id="right" src="images/PiStation3.jpg">
+    </div>
+  </body>	
+  </div>
+</html>
